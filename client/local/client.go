64d66e22182a75c2d8ed8b6e74af27a9692696d1
drakos74 <drakos74@gmail.com>
2021-02-20 13:06:42 +0100

    fix build

--- client/local/client.go
+++ client/local/client.go
@@ -16,22 +16,22 @@ import (
 // furthermore it will mock the client behaviour in terms of the positions and orders locally.
 // It can be used as a simulation environment for testing processors and business logic.
 type Client struct {
-	upstream    model.TradeClient
+	upstream    api.TradeClient
 	persistence storage.Persistence
 }
 
-func (c Client) Trades(stop <-chan struct{}, coin api.Coin, stopExecution api.Condition) (api.TradeSource, error) {
+func (c Client) Trades(stop <-chan struct{}, coin model.Coin, stopExecution api.Condition) (model.TradeSource, error) {
 	panic("implement me")
 }
 
-func (c Client) OpenPositions(ctx context.Context) (*api.PositionBatch, error) {
+func (c Client) OpenPositions(ctx context.Context) (*model.PositionBatch, error) {
 	panic("implement me")
 }
 
-func (c Client) OpenPosition(position api.Position) error {
+func (c Client) OpenPosition(position model.Position) error {
 	panic("implement me")
 }
 
-func (c Client) ClosePosition(position api.Position) error {
+func (c Client) ClosePosition(position model.Position) error {
 	panic("implement me")
 }
