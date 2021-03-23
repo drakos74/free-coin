815842b2cc6107b763034b2fa9ff9c7bed501037
drakos74 <drakos74@gmail.com>
2021-03-23 09:12:31 +0100

    remove debug messages for server body parsing

--- internal/algo/processor/external/process.go
+++ internal/algo/processor/external/process.go
@@ -12,7 +12,7 @@ import (
 func handle(user api.User) server.Handler {
 	return func(ctx context.Context, r *http.Request) ([]byte, int, error) {
 		var message Message
-		payload, jsonErr := server.ReadJson(r, true, &message)
+		payload, jsonErr := server.ReadJson(r, false, &message)
 		if jsonErr != nil {
 			user.Send(api.Private, api.NewMessage(fmt.Sprintf("error = %v \n raw = %+v", jsonErr.Error(), payload)), nil)
 			return []byte{}, http.StatusOK, nil
