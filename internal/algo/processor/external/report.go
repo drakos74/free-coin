e0553dfe35d2fd40616e2efe075c2849c2bdf887
drakos74 <drakos74@gmail.com>
2021-03-29 23:35:05 +0200

    refactor reporting

--- internal/algo/processor/external/report.go
+++ internal/algo/processor/external/report.go
@@ -1,11 +1,15 @@
 package external
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
 	"sort"
 	"strings"
+	"time"
+
+	"github.com/drakos74/free-coin/internal/api"
 
 	"github.com/drakos74/free-coin/internal/metrics"
 	"github.com/drakos74/free-coin/internal/model"
@@ -14,22 +18,29 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func addTargets(grafana *metrics.Server, registry storage.Registry) {
+func addTargets(client api.Exchange, grafana *metrics.Server, registry storage.Registry) {
 	registryPath := filepath.Join(storage.DefaultDir, storage.RegistryDir, storage.SignalsPath)
 	// TODO : get current prices ...
 
-	grafana.Target("PnL", readFromRegistry(registryPath, registry, noError, addPnL))
+	grafana.Target("PnL", readFromRegistry(client, registryPath, registry, noError, addPnL))
 
-	grafana.Target("trades", readFromRegistry(registryPath, registry, noError, count))
+	grafana.Target("trades", readFromRegistry(client, registryPath, registry, noError, count))
 
-	grafana.Target("errors", readFromRegistry(registryPath, registry, isError, count))
+	grafana.Target("errors", readFromRegistry(client, registryPath, registry, isError, count))
 }
 
-func readFromRegistry(registryPath string, registry storage.Registry, condition func(dir string) bool, addSeries func(index string, orders Orders) metrics.Series) func(data map[string]interface{}) []metrics.Series {
+func readFromRegistry(client api.Exchange, registryPath string, registry storage.Registry, condition func(dir string) bool, addSeries func(index string, prices map[model.Coin]model.CurrentPrice, orders Orders) metrics.Series) func(data map[string]interface{}) []metrics.Series {
 	return func(data map[string]interface{}) []metrics.Series {
 		assets := make([]metrics.Series, 0)
 		accountFor := make(map[string]struct{})
-		err := filepath.Walk(registryPath, func(path string, info os.FileInfo, err error) error {
+
+		prices, err := client.CurrentPrice(context.Background())
+
+		if err != nil {
+			prices = make(map[model.Coin]model.CurrentPrice)
+		}
+
+		err = filepath.Walk(registryPath, func(path string, info os.FileInfo, err error) error {
 			// take into account only files
 			dir := filepath.Dir(path)
 			index := filepath.Base(dir)
@@ -37,7 +48,7 @@ func readFromRegistry(registryPath string, registry storage.Registry, condition 
 				if _, ok := accountFor[index]; ok {
 					return nil
 				}
-				series, err := parseEvents(dir, index, registry, condition, addSeries)
+				series, err := parseEvents(dir, index, registry, prices, condition, addSeries)
 				if err == nil {
 					assets = append(assets, series)
 					accountFor[index] = struct{}{}
@@ -55,7 +66,7 @@ func readFromRegistry(registryPath string, registry storage.Registry, condition 
 	}
 }
 
-func parseEvents(dir string, index string, registry storage.Registry, condition func(dir string) bool, addSeries func(index string, orders Orders) metrics.Series) (metrics.Series, error) {
+func parseEvents(dir string, index string, registry storage.Registry, prices map[model.Coin]model.CurrentPrice, condition func(dir string) bool, addSeries func(index string, prices map[model.Coin]model.CurrentPrice, orders Orders) metrics.Series) (metrics.Series, error) {
 	if condition(dir) {
 
 		orders := []Order{{}}
@@ -72,18 +83,19 @@ func parseEvents(dir string, index string, registry storage.Registry, condition 
 		sortedOrders = orders
 		sort.Sort(sortedOrders)
 
-		return addSeries(index, sortedOrders), nil
+		return addSeries(index, prices, sortedOrders), nil
 	}
 	return metrics.Series{}, fmt.Errorf("invalid dir")
 }
 
-func addPnL(index string, orders Orders) metrics.Series {
+func addPnL(index string, prices map[model.Coin]model.CurrentPrice, orders Orders) metrics.Series {
 	series := metrics.Series{
 		Target:     index,
 		DataPoints: make([][]float64, 0),
 	}
 	sum := 0.0
 	var close bool
+	var lastOrder Order
 	for _, order := range orders {
 		switch order.Order.Type {
 		case model.Buy:
@@ -95,12 +107,28 @@ func addPnL(index string, orders Orders) metrics.Series {
 		if close {
 			series.DataPoints = append(series.DataPoints, []float64{sum, float64(cointime.ToMilli(order.Order.Time))})
 		}
+		lastOrder = order
 		close = !close
+	}
+	// if we are at the last one .. we ll add a virtual one at the current price
+	if close {
+		if p, ok := prices[lastOrder.Order.Coin]; ok {
+			// if we have a last price for this asset ...
+			var diff float64
+			switch lastOrder.Order.Type {
+			case model.Buy:
+				diff = p.Price - lastOrder.Order.Price
+			case model.Sell:
+				diff = lastOrder.Order.Price - p.Price
+			}
+			sum += lastOrder.Order.Volume * diff
+			series.DataPoints = append(series.DataPoints, []float64{sum, float64(cointime.ToMilli(time.Now()))})
+		}
 	}
 	return series
 }
 
-func count(index string, orders Orders) metrics.Series {
+func count(index string, prices map[model.Coin]model.CurrentPrice, orders Orders) metrics.Series {
 	count := 0.0
 
 	series := metrics.Series{
@@ -114,7 +142,7 @@ func count(index string, orders Orders) metrics.Series {
 	return series
 }
 
-func instance(index string, orders Orders) metrics.Series {
+func instance(index string, prices map[model.Coin]model.CurrentPrice, orders Orders) metrics.Series {
 	series := metrics.Series{
 		Target:     index,
 		DataPoints: make([][]float64, 0),
