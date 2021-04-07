cef8d0d3f520c0caef59983bf3aa2c331680f145
drakos74 <drakos74@gmail.com>
2021-04-07 20:56:06 +0200

    enable start / stop

--- internal/algo/processor/external/signal.go
+++ internal/algo/processor/external/signal.go
@@ -53,7 +53,7 @@ func (t *tracker) switchOnOff(user api.User) {
 		case "stop":
 			t.running = false
 		}
-		api.Reply(api.Index(command.User), user, api.NewMessage(processor.Audit(t.compoundKey(ProcessorName), fmt.Sprintf("running: %s", emoji.MapBool(t.running)))).ReplyTo(command.ID), nil)
+		api.Reply(api.Index(command.User), user, api.NewMessage(processor.Audit(t.compoundKey(ProcessorName), emoji.MapBool(t.running))).ReplyTo(command.ID), nil)
 
 	}
 }
