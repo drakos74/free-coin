60094af99a4e8f315d1cf0e638c7d5c3a033479f
drakos74 <drakos74@gmail.com>
2021-04-07 20:31:17 +0200

    log incoming message

--- internal/algo/processor/external/process.go
+++ internal/algo/processor/external/process.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/rs/zerolog/log"
+
 	"github.com/drakos74/free-coin/internal/api"
 	"github.com/drakos74/free-coin/internal/server"
 )
@@ -17,6 +19,7 @@ func handle(user api.User, signal chan<- Message) server.Handler {
 			user.Send(api.External, api.NewMessage(fmt.Sprintf("error = %v \n raw = %+v", jsonErr.Error(), payload)), nil)
 			return []byte{}, http.StatusBadRequest, nil
 		}
+		log.Debug().Str("message", fmt.Sprintf("%+v", message)).Msg("signal received")
 		if signal != nil {
 			signal <- message
 		}
