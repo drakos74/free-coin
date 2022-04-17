4c8094b9db919253b4fd4a570bbc0a21fdd90827
drakos74 <drakos74@gmail.com>
2022-04-17 12:23:31 +0200

    fix ds cache size

--- internal/algo/processor/ml/net/dataset.go
+++ internal/algo/processor/ml/net/dataset.go
@@ -85,7 +85,7 @@ func (ds *Datasets) Push(key model.Key, vv mlmodel.Vector, cfg mlmodel.Model) (*
 		ds.sets[key] = newDataSet(key.Coin, key.Duration, cfg, vectors, ds.network(cfg))
 	}
 	// keep only the last Vectors based on the buffer size + 10% to cover for the max
-	bufferSize := cfg.BufferSize + cfg.BufferSize/10
+	bufferSize := cfg.BufferSize + cfg.BufferSize/10 + 1
 	newVectors := addVector(ds.sets[key].Vectors, vv, bufferSize)
 	err := ds.saveVectors(key, newVectors)
 	if err != nil {
