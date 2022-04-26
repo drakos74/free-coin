6b26ac61a55616218376078c96948197606aa971
drakos74 <drakos74@gmail.com>
2022-04-27 01:36:46 +0200

    remove hmm network

--- cmd/free-coin/free-coin.go
+++ cmd/free-coin/free-coin.go
@@ -83,12 +83,11 @@ func mlProcessor(u api.User, e api.Exchange, shard storage.Shard, registry stora
 	return coin.NewStrategy(ml.Name).
 		ForUser(u).
 		ForExchange(e).
-		WithProcessor(ml.Processor(api.FreeCoin, shard, registry,
-			configML(),
+		WithProcessor(ml.Processor(api.FreeCoin, shard, registry, configML(),
 			net.ConstructRandomForest(false),
 			net.ConstructRandomForest(false),
 			net.ConstructRandomForest(false),
-			net.ConstructHMM(),
+			//net.ConstructHMM(),
 		)).
 		Apply()
 }
